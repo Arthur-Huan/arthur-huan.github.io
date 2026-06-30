5f397db0b289c2aabc88b362cc2cf58707d997a8
Arthur Huan <Arthur-Huan@users.noreply.github.com>
2026-06-30 17:00:35 -0600

    First commit

---
+++ src/data/projects.js
@@ -0,0 +1,14 @@
+export const projects = [
+  {
+    slug: 'course-tree',
+    title: 'CourseTree',
+    summary: '',
+    thumbnail: '/images/course-tree/tree.png',
+  },
+  {
+    slug: 'rna-seq-pipeline',
+    title: 'RNA Seq Pipeline',
+    summary: '',
+    thumbnail: '/images/rna-seq-pipeline/volcano.png',
+  },
+];
