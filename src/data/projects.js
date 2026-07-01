e8f19f21a0b190a4bf004890a3e3ba0a96c54f42
Arthur Huan <Arthur-Huan@users.noreply.github.com>
2026-06-30 20:31:26 -0600

    Remove and hide unfinished project pages

--- src/data/projects.js
+++ src/data/projects.js
@@ -5,22 +5,4 @@ export const projects = [
     summary: 'Raw fastq reads -> FastQC -> Salmon -> tximport -> DESeq2 -> Results & Visualizations',
     thumbnail: '/images/rna-seq-pipeline/volcano.png',
   },
-  {
-    slug: 'market-simulation',
-    title: 'Market Simulation',
-    summary: '',
-    thumbnail: '',
-  },
-  {
-    slug: 'computer-vision',
-    title: 'Computer Vision with Machine Learning',
-    summary: '',
-    thumbnail: '',
-  },
-  {
-    slug: 'transport-data-analysis',
-    title: 'Calgary transport data analysis',
-    summary: '',
-    thumbnail: '',
-  },
 ];
