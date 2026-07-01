8b75a776bb924aa749cf1006d4b946f72207f785
Arthur Huan <Arthur-Huan@users.noreply.github.com>
2026-06-30 19:45:43 -0600

    WIP, move CourseTree up to hero project

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,14 +1,14 @@
 export const projects = [
   {
-    slug: 'course-tree',
-    title: 'CourseTree',
-    summary: '',
-    thumbnail: '/images/course-tree/tree.png',
-  },
-  {
     slug: 'rna-seq-pipeline',
     title: 'RNA Seq Pipeline',
-    summary: '',
+    summary: 'Raw fastq reads -> FastQC -> Salmon -> tximport -> DESeq2 -> Results & Visualizations',
     thumbnail: '/images/rna-seq-pipeline/volcano.png',
+  },
+  {
+    slug: 'market-simulation',
+    title: 'Market Simulation',
+    summary: '',
+    thumbnail: '',
   },
 ];
